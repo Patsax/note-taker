b6804356be6e9123de2c0977c7deb5e6f05e5af0
Patrick <patasax22@yahoo.com>
2021-10-19 18:26:32 -0400

    front end connected, tyring to figure out post routes

--- routes/api/apiRoutes.js
+++ routes/api/apiRoutes.js
@@ -1,8 +1,13 @@
 const router = require("express").Router();
+const fs = require('fs');
 const notes = require('../../db/db.json')
 
-router.get("/notes", (req, res) => {
+router.get("/api/notes", (req, res) => {
     res.json(notes);
 });
+
+router.post("/api/notes", (req, res) => {
+    req.body;
+})
 
 module.exports = router;
