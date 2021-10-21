e590ee6ac4d5aab3c4ccb85bfb1f93e890323963
Patrick <patasax22@yahoo.com>
2021-10-21 17:10:41 -0400

    note display figured out

--- routes/apiRoutes.js
+++ routes/apiRoutes.js
@@ -1,6 +1,6 @@
 const express = require("express")
 const app = express();
-const { v4: uuidv4 } = require('uuid');
+const { uuid } = require('uuidv4');
 const fs = require('fs');
 const notes = require('../db/db.json');
 
@@ -12,7 +12,7 @@ app.post("/notes", (req, res) => {
     // req.body IS the note i want to save
     const newNote = req.body;
     // uuid(universally unique identifier) for adding an id 
-    newNote.id = uuidv4;
+    newNote.id = uuid();
     // adds new note file db.json
     notes.push(newNote);
     fs.writeFile(__dirname + '/../db/db.json', JSON.stringify(notes), function (error) {
